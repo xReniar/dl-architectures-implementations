60396400bae47081a69cf0995cae0f351b6b9f7f
xReniar <ilaorainer@gmail.com>
2024-12-03 18:27:58 +0100

    Add VGG-16 implementation

---
+++ models/VGG-16/model.py
@@ -0,0 +1,70 @@
+from torch import nn
+import torch
+
+
+class VGG_16(nn.Module):
+    def __init__(self, num_classes) -> None:
+        super(VGG_16, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3,64,kernel_size=3, stride=1,padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64,64,kernel_size=3, stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64,128,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128,128,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128,256,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256,256,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(256,512,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,stride=2)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(25088, 4096),
+            nn.ReLU()
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(4096, 4096),
+            nn.ReLU()
+        )
+        self.fc3 = nn.Sequential(
+            nn.Linear(4096,num_classes)
+        )
+
+    def forward(self, x:torch.Tensor):
+        out:torch.Tensor = x
+        out = self.conv1(out)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.conv5(out)
+        out = out.view(out.size(0),-1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
