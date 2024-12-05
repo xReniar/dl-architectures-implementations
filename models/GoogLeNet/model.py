f8694dcb7a50805124626d892f29ae67838e6391
xReniar <ilaorainer@gmail.com>
2024-12-05 23:11:05 +0100

    Start implementation of GoogLeNet

---
+++ models/GoogLeNet/model.py
@@ -0,0 +1,13 @@
+from torch import nn
+import torch
+
+def Inception_block():
+    pass
+
+class GoogLeNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def forward(self, x:torch.Tensor):
+        pass
