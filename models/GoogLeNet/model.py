9555d70aede6ae5abcdf7bd1ce97b91bb25607fa
xReniar <ilaorainer@gmail.com>
2024-12-07 18:25:16 +0100

    Remove functions that return inception block given version

--- models/GoogLeNet/model.py
+++ models/GoogLeNet/model.py
@@ -1,25 +1,23 @@
+from inception_v1 import Inception_Block_v1
+from inception_v2 import Inception_Block_v2
+from inception_v3 import Inception_Block_v3
+from inception_v4 import Inception_Block_v4
 from torch import nn
 import torch
 
-def inception_block_v1():
-    pass
-
-def inception_block_v2():
-    pass
-
-def inception_block_v3():
-    pass
-
-def inception_block_v4():
-    pass
 
 def auxiliary_classifier():
     pass
 
 class GoogLeNet(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, num_classes:int, inception_block_version:str):
         super().__init__()
-
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7,stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+        self.inception_1 = Inception_Block_v1()
 
     def forward(self, x:torch.Tensor):
         pass
