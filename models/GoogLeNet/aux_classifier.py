5770039d4548f696d4dab57cd814d9fe1957a4cf
xReniar <ilaorainer@gmail.com>
2024-12-11 19:55:25 +0100

    Add sequential module and modified input value of fc1

--- models/GoogLeNet/aux_classifier.py
+++ models/GoogLeNet/aux_classifier.py
@@ -6,10 +6,19 @@ class AuxClassifier(nn.Module):
     def __init__(self, in_features, conv_out, fc_out, num_classes):
         super().__init__()
         self.avgpool = nn.AvgPool2d(kernel_size=5, stride=3)
-        self.conv = nn.Conv2d(in_features, conv_out, kernel_size=1, stride=1)
-        self.fc1 = nn.Linear(conv_out, fc_out)
-        self.fc2 = nn.Linear(fc_out, num_classes)
-        self.softmax = nn.Softmax(dim=1)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_features, conv_out, kernel_size=1, stride=1),
+            nn.BatchNorm2d(conv_out),
+            nn.ReLU()
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(2048, fc_out),
+            nn.ReLU()
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(fc_out, num_classes),
+            nn.Softmax(dim=1)
+        )
 
     def forward(self, x:torch.Tensor):
         out: torch.Tensor = x
@@ -18,6 +27,5 @@ class AuxClassifier(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.fc1(out)
         out = self.fc2(out)
-        out = self.softmax(out)
         
         return out
