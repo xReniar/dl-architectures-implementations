f7461e1b71997a7f4dc948ccb8c7be3da5f78380
xReniar <ilaorainer@gmail.com>
2024-12-11 15:01:46 +0100

    Add auxiliary classifier and fix bug

--- models/GoogLeNet/InceptionModule/v1.py
+++ models/GoogLeNet/InceptionModule/v1.py
@@ -2,20 +2,17 @@ from torch import nn
 import torch
 
 
-def auxiliary_classifier():
-    pass
-
 class Inception_Block_v1(nn.Module):
     def __init__(self, *args):
         super().__init__()
         # define features values for every branch
         self.in_features = args[0]
-        self._1x1 = args[1][0]
-        self._3x3_reduce = args[2][0]
-        self._3x3 = args[2][1]
-        self._5x5_reduce = args[3][0]
-        self._5x5 = args[3][1]
-        self.pool_proj = args[4][0]
+        self._1x1 = args[1]
+        self._3x3_reduce = args[2]
+        self._3x3 = args[3]
+        self._5x5_reduce = args[4]
+        self._5x5 = args[5]
+        self.pool_proj = args[6]
 
         self.branch1 = nn.Sequential(
             nn.Conv2d(self.in_features, self._1x1, kernel_size=1),
@@ -43,11 +40,16 @@ class Inception_Block_v1(nn.Module):
             nn.BatchNorm2d(self.pool_proj),
             nn.ReLU()
         )
+        self.aux_classifier = args[7]
+        self.aux_value = None
 
     def forward(self, x:torch.Tensor):
         b1_out = self.branch1(x)
         b2_out = self.branch2(x)
         b3_out = self.branch3(x)
         b4_out = self.branch4(x)
+        
+        if self.aux_classifier != None:
+            self.aux_value = self.aux_classifier(x)
 
         return torch.cat([b1_out, b2_out, b3_out, b4_out], dim=1)
