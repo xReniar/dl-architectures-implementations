f35a5be6a8feb1e7d37f2d6dc589405568600e9f
xReniar <ilaorainer@gmail.com>
2024-12-12 23:25:25 +0100

    Add Resnet, ResneXt and Densenet

--- models/Densenet/model.py
+++ models/Densenet/model.py
@@ -2,7 +2,7 @@ from torch import nn
 import torch
 
 
-class Resnet(nn.Module):
+class Densenet(nn.Module):
     def __init__(self):
         super().__init__()
 
--- models/ResneXt/model.py
+++ models/ResneXt/model.py
@@ -2,7 +2,7 @@ from torch import nn
 import torch
 
 
-class Resnet(nn.Module):
+class ResneXt(nn.Module):
     def __init__(self):
         super().__init__()
 
