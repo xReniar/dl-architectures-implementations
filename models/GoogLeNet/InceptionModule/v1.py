4b58c3f8cd7bda925d4a2b19491f9acc198a4acb
xReniar <ilaorainer@gmail.com>
2024-12-14 19:48:44 +0100

    Move aux_value inside aux_classifier

--- models/GoogLeNet/InceptionModule/v1.py
+++ models/GoogLeNet/InceptionModule/v1.py
@@ -6,13 +6,13 @@ class InceptionModulev1(nn.Module):
     def __init__(self, *args):
         super().__init__()
         # define features values for every branch
-        self.in_features = args[0]
-        self._1x1 = args[1]
-        self._3x3_reduce = args[2]
-        self._3x3 = args[3]
-        self._5x5_reduce = args[4]
-        self._5x5 = args[5]
-        self.pool_proj = args[6]
+        self.in_features:int = args[0]
+        self._1x1:int = args[1]
+        self._3x3_reduce:int = args[2]
+        self._3x3:int = args[3]
+        self._5x5_reduce:int = args[4]
+        self._5x5:int = args[5]
+        self.pool_proj:int = args[6]
 
         self.branch1 = nn.Sequential(
             nn.Conv2d(self.in_features, self._1x1, kernel_size=1),
@@ -41,8 +41,7 @@ class InceptionModulev1(nn.Module):
             nn.BatchNorm2d(self.pool_proj),
             nn.ReLU()
         )
-        self.aux_classifier = args[7]
-        self.aux_value = None
+        self.aux_classifier:nn.Module = args[7]
 
     def forward(self, x:torch.Tensor, mode:bool = False):
         b1_out = self.branch1(x)
@@ -51,6 +50,6 @@ class InceptionModulev1(nn.Module):
         b4_out = self.branch4(x)
         
         if self.aux_classifier != None and mode:
-            self.aux_value = self.aux_classifier(x)
+            self.aux_classifier(x)
 
         return torch.cat([b1_out, b2_out, b3_out, b4_out], dim=1)
