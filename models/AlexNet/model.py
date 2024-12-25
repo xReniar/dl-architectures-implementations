ba7c95a7f412611aa6f9e96d9a3d4ef7c3a9750f
xReniar <ilaorainer@gmail.com>
2024-12-25 23:36:15 +0100

    Add batchnorm to speed up the training

--- models/AlexNet/model.py
+++ models/AlexNet/model.py
@@ -6,24 +6,29 @@ class AlexNet(nn.Module):
         super().__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 96, kernel_size=11,stride=4),
+            nn.BatchNorm2d(96),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.conv2 = nn.Sequential(
             nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=2),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.conv3 = nn.Sequential(
             nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
             nn.ReLU(inplace=True)
         )
         self.conv4 = nn.Sequential(
             nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
             nn.ReLU(inplace=True)
         )
         self.conv5 = nn.Sequential(
             nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size = 3, stride = 2)
         )
