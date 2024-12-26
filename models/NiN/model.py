16fd229ba1d0c26601801fa8420653a63154c336
xReniar <ilaorainer@gmail.com>
2024-12-26 15:14:56 +0100

    Modify NiN_block() implementation to pass more out_features values, Adjust NiN class for 224x224x3 input size, Add flatten to return correct size tensor

--- models/NiN/model.py
+++ models/NiN/model.py
@@ -2,33 +2,30 @@ from torch import nn
 import torch
 
 
-def NiN_block(in_features:int, out_features:int, kernel_size:int, stride:int = 1, padding:int = 0):
+def NiN_block(in_features:int, out_features:list, kernel_size:int, stride:int = 1, padding:int = 0):
     return nn.Sequential(
-        nn.Conv2d(in_features, out_features, kernel_size=kernel_size, stride=stride, padding=padding),
+        nn.Conv2d(in_features, out_features[0], kernel_size=kernel_size, stride=stride, padding=padding),
         nn.ReLU(inplace=True),
-        nn.Conv2d(out_features, out_features, kernel_size=1),
+        nn.Conv2d(out_features[0], out_features[1], kernel_size=1),
         nn.ReLU(inplace=True),
-        nn.Conv2d(out_features, out_features, kernel_size=1),
+        nn.Conv2d(out_features[1], out_features[2], kernel_size=1),
         nn.ReLU(inplace=True)
     )
 
 class NiN(nn.Module):
     def __init__(self, num_classes) -> None:
         super().__init__()
+        self.num_classes = num_classes
         # first block
-        self.nin_block1 = NiN_block(in_features=3, out_features=96, kernel_size=11, stride=4)
-        self.pool1 = nn.MaxPool2d(kernel_size=3,stride=2)
+        self.nin_block1 = NiN_block(in_features=3, out_features=[384, 192, 96], kernel_size=5, stride=1, padding=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=3,stride=2, padding=1)
 
         # second block
-        self.nin_block2 = NiN_block(in_features=96, out_features=256, kernel_size=5, padding=2)
-        self.pool2 = nn.MaxPool2d(kernel_size=3,stride=2)
+        self.nin_block2 = NiN_block(in_features=96, out_features=[384, 192, 192], kernel_size=5, stride=1, padding=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=3,stride=2, padding=1)
 
         # third block
-        self.nin_block3 = NiN_block(in_features=256, out_features=384, kernel_size=3, padding=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=3,stride=2)
-
-        # fourth block
-        self.nin_block4 = NiN_block(in_features=384, out_features=num_classes, kernel_size=3, padding=1)
+        self.nin_block3 = NiN_block(in_features=192, out_features=[192, 192, self.num_classes], kernel_size=3, padding=1)
         
         # global average poooling
         self.gap = nn.AdaptiveAvgPool2d((1,1))
@@ -40,8 +37,8 @@ class NiN(nn.Module):
         out = self.nin_block2(out)
         out = self.pool2(out)
         out = self.nin_block3(out)
-        out = self.pool3(out)
-        out = self.nin_block4(out)
         out = self.gap(out)
+
+        out = torch.flatten(out, 1)
 
         return out
