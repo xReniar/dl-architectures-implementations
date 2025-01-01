1f463899ba9fc9a38e33a776ef8cc4f03d048346
xReniar <ilaorainer@gmail.com>
2025-01-01 02:27:52 +0100

    Add batchnorm and set bias to false, Correct conv3 layer

--- models/Vgg16/model.py
+++ models/Vgg16/model.py
@@ -6,41 +6,56 @@ class vgg16(nn.Module):
     def __init__(self, num_classes) -> None:
         super().__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3,64,kernel_size=3, stride=1,padding=1),
+            nn.Conv2d(3,64,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64,64,kernel_size=3, stride=1,padding=1),
+            nn.Conv2d(64,64,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(64,128,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64,128,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128,128,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(128,128,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(128,256,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(128,256,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256,256,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(256,256,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(256,512,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(256,512,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
-            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
-            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2,stride=2)
         )
         self.conv5 = nn.Sequential(
-            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
-            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
-            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(512,512,kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
