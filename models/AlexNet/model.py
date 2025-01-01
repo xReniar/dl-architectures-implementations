4af42b01f4e10825854b21214d2e9a8f45320bb6
xReniar <ilaorainer@gmail.com>
2025-01-01 02:00:34 +0100

    Set bias to false for conv layers before batchnorm

--- models/AlexNet/model.py
+++ models/AlexNet/model.py
@@ -5,29 +5,29 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes:int) -> None:
         super().__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11,stride=4),
+            nn.Conv2d(3, 96, kernel_size=11,stride=4, bias=False),
             nn.BatchNorm2d(96),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=2),
+            nn.Conv2d(96, 256, kernel_size=3, stride=1, padding=2, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1,bias=False),
             nn.BatchNorm2d(384),
             nn.ReLU(inplace=True)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(384),
             nn.ReLU(inplace=True)
         )
         self.conv5 = nn.Sequential(
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size = 3, stride = 2)
