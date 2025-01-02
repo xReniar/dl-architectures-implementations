65ee113eeedb6c78340f4eaf073fd8b7904b6656
xReniar <ilaorainer@gmail.com>
2025-01-02 23:10:59 +0100

    Start implementing Densenet

--- models/Densenet/model.py
+++ models/Densenet/model.py
@@ -1,10 +1,47 @@
+from densenet_block import DenseBlock
 from torch import nn
 import torch
 
 
-class Densenet121(nn.Module):
-    def __init__(self):
+class Densenet(nn.Module):
+    def __init__(self, num_classes:int):
         super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.dense1 = DenseBlock()
+        self.transition1 = nn.Sequential(
+            nn.Conv2d(1, 1, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(1),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+
+        self.dense2 = DenseBlock()
+        self.transition2 = nn.Sequential(
+            nn.Conv2d(1, 1, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(1),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+
+        self.dense3 = DenseBlock()
+        self.transition3 = nn.Sequential(
+            nn.Conv2d(1, 1, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(1),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+
+        self.dense4 = DenseBlock()
+        self.gap = nn.AdaptiveAvgPool2d((1,1))
+        self.classifier = nn.Linear(10, num_classes)
 
     def forward(self, x:torch.Tensor):
-        pass
+        x = self.conv(x)
+
+        return x
