1aee4250ec511557fb121a3261f8c1ec86e9964f
xReniar <ilaorainer@gmail.com>
2025-01-05 02:06:49 +0100

    Modify Resnet class to pass only num_classes and add functions for every variant

--- models/Resnet/model.py
+++ models/Resnet/model.py
@@ -1,55 +1,26 @@
-from .residual_block import ResidualBlock
+from residual_block import ResidualBlock
 from torch import nn
 import torch
     
 
 class Resnet(nn.Module):
-    def __init__(self, layers:int, num_classes:int):
+    def __init__(self, conv_configurations:list, num_classes:int):
         super().__init__()
-
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
-
-        if layers == 18:
-            self.conv2_x = self._make_layer(64, 2, [(64, 3), (64, 3)], stride=1)
-            self.conv3_x = self._make_layer(64, 2, [(128, 3), (128, 3)], stride=2)
-            self.conv4_x = self._make_layer(128, 2, [(256, 3), (256, 3)], stride=2)
-            self.conv5_x = self._make_layer(256, 2, [(512, 3), (512, 3)], stride=2)
-        elif layers == 34:
-            self.conv2_x = self._make_layer(64, 3, [(64, 3), (64, 3)], stride=1)
-            self.conv3_x = self._make_layer(64, 4, [(128, 3), (128, 3)], stride=2)
-            self.conv4_x = self._make_layer(128, 6, [(256, 3), (256, 3)], stride=2)
-            self.conv5_x = self._make_layer(256, 3, [(512, 3), (512, 3)], stride=2)
-        elif layers == 50:
-            self.conv2_x = self._make_layer(64, 3, [(64, 1), (64, 3), (256, 1)], stride=1)
-            self.conv3_x = self._make_layer(256, 4, [(128, 1), (128, 3), (512, 1)], stride=2)
-            self.conv4_x = self._make_layer(512, 6, [(256, 1), (256, 3), (1024, 1)], stride=2)
-            self.conv5_x = self._make_layer(1024, 3, [(512, 1), (512, 3), (2048, 1)], stride=2)
-        elif layers == 101:
-            self.conv2_x = self._make_layer(64, 3, [(64, 1), (64, 3), (256, 1)], stride=1)
-            self.conv3_x = self._make_layer(256, 4, [(128, 1), (128, 3), (512, 1)], stride=2)
-            self.conv4_x = self._make_layer(512, 23, [(256, 1), (256, 3), (1024, 1)], stride=2)
-            self.conv5_x = self._make_layer(1024, 3, [(512, 1), (512, 3), (2048, 1)], stride=2)
-        elif layers == 152:
-            self.conv2_x = self._make_layer(64, 3, [(64, 1), (64, 3), (256, 1)], stride=1)
-            self.conv3_x = self._make_layer(256, 8, [(128, 1), (128, 3), (512, 1)], stride=2)
-            self.conv4_x = self._make_layer(512, 36, [(256, 1), (256, 3), (1024, 1)], stride=2)
-            self.conv5_x = self._make_layer(1024, 3, [(512, 1), (512, 3), (2048, 1)], stride=2)
-        else:
-            raise Exception(f"resnet{layers} does not exists")
-            
-
+        self.conv2_x = self._make_layer(*conv_configurations[0])
+        self.conv3_x = self._make_layer(*conv_configurations[1])
+        self.conv4_x = self._make_layer(*conv_configurations[2])
+        self.conv5_x = self._make_layer(*conv_configurations[3])
         self.gap = nn.AdaptiveAvgPool2d((1,1))
-        
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * (4 if layers > 34 else 1), num_classes)
-        )
+        self.classifier = nn.Linear(512 * (4 if len(conv_configurations[3][2]) > 2 else 1), num_classes)
 
-    def _make_layer(self, in_features:int, expansion:int, building_blocks:list, stride:int) -> nn.Sequential:
+    def _make_layer(self, *args) -> nn.Sequential:
+        in_features, expansion, building_blocks, stride = args[0], args[1], args[2], args[3]
         layers = []
 
         feature_map_list = self._create_in_feature_maps_list(in_features, expansion, building_blocks)
@@ -88,3 +59,49 @@ class Resnet(nn.Module):
         out = self.classifier(out)
 
         return out
+    
+    
+def resnet18(num_classes:int):
+    configurations = [
+        (64, 2, [(64, 3), (64, 3)], 1),
+        (64, 2, [(128, 3), (128, 3)], 2),
+        (128, 2, [(256, 3), (256, 3)], 2),
+        (256, 2, [(512, 3), (512, 3)], 2)
+    ]
+    return Resnet(configurations, num_classes)
+
+def resnet34(num_classes:int):
+    configurations = [
+        (64, 3, [(64, 3), (64, 3)], 1),
+        (64, 4, [(128, 3), (128, 3)], 2),
+        (128, 6, [(256, 3), (256, 3)], 2),
+        (256, 3, [(512, 3), (512, 3)], 2)
+    ]
+    return Resnet(configurations, num_classes)
+
+def resnet50(num_classes:int):
+    configurations = [
+        (64, 3, [(64, 1), (64, 3), (256, 1)], 1),
+        (256, 4, [(128, 1), (128, 3), (512, 1)], 2),
+        (512, 6, [(256, 1), (256, 3), (1024, 1)], 2),
+        (1024, 3, [(512, 1), (512, 3), (2048, 1)], 2)
+    ]
+    return Resnet(configurations, num_classes)
+
+def resnet101(num_classes:int):
+    configurations = [
+        (64, 3, [(64, 1), (64, 3), (256, 1)], 1),
+        (256, 4, [(128, 1), (128, 3), (512, 1)], 2),
+        (512, 23, [(256, 1), (256, 3), (1024, 1)], 2),
+        (1024, 3, [(512, 1), (512, 3), (2048, 1)], 2)
+    ]
+    return Resnet(configurations, num_classes)
+
+def resnet152(num_classes:int):
+    configurations = [
+        (64, 3, [(64, 1), (64, 3), (256, 1)], 1),
+        (256, 8, [(128, 1), (128, 3), (512, 1)], 2),
+        (512, 36, [(256, 1), (256, 3), (1024, 1)], 2),
+        (1024, 3, [(512, 1), (512, 3), (2048, 1)], 2)
+    ]
+    return Resnet(configurations, num_classes)
