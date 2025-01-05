e9f99459ba469311b88f66722207f645507c23a3
xReniar <ilaorainer@gmail.com>
2025-01-05 21:40:10 +0100

    Finish DenseNet implementation, at the moment only growth_rate set to 32 works

--- models/Densenet/model.py
+++ models/Densenet/model.py
@@ -1,13 +1,18 @@
-from densenet_block import DenseBlock
+from .densenet_block import DenseBlock
 from torch import nn
 import torch
 
 
 class DenseNet(nn.Module):
-    _growth_rate = 32
+    # only works with 32
+    __growth_rate = 32
 
-    def __init__(self, num_classes:int):
+    def __init__(self, expansions:list, num_classes:int):
         super().__init__()
+        self.__transition3_in = 256 + (expansions[2] * self.__growth_rate)
+        self.__classifier_in = (self.__transition3_in // 2) + (expansions[3] * self.__growth_rate)
+
+
         self.conv = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=2, bias=False),
             nn.BatchNorm2d(64),
@@ -15,48 +20,67 @@ class DenseNet(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
 
-        self.dense1 = DenseBlock()
+        self.dense1 = DenseBlock(64, expansions[0], self.__growth_rate)
         self.transition1 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=1, stride=2, bias=False),
+            nn.Conv2d(256, 128, kernel_size=1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=2, stride=2)
         )
 
-        self.dense2 = DenseBlock()
+        self.dense2 = DenseBlock(128, expansions[1], self.__growth_rate)
         self.transition2 = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=1, stride=2, bias=False),
-            nn.BatchNorm2d(1),
+            nn.Conv2d(512, 256, kernel_size=1, bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=2, stride=2)
         )
 
-        self.dense3 = DenseBlock()
+        self.dense3 = DenseBlock(256, expansions[2], self.__growth_rate)
         self.transition3 = nn.Sequential(
-            nn.Conv2d(256 , 1, kernel_size=1, stride=2, bias=False),
-            nn.BatchNorm2d(1),
+            nn.Conv2d(self.__transition3_in, self.__transition3_in // 2, kernel_size=1, bias=False),
+            nn.BatchNorm2d(self.__transition3_in // 2),
             nn.ReLU(inplace=True),
             nn.AvgPool2d(kernel_size=2, stride=2)
         )
 
-        self.dense4 = DenseBlock()
+        self.dense4 = DenseBlock(self.__transition3_in // 2, expansions[3], self.__growth_rate)
         self.gap = nn.AdaptiveAvgPool2d((1,1))
-        self.classifier = nn.Linear(10, num_classes)
+        self.classifier = nn.Linear(self.__classifier_in, num_classes)
 
     def forward(self, x:torch.Tensor):
         x = self.conv(x)
+        x = self.dense1(x)
+        x = self.transition1(x)
+        x = self.dense2(x)
+        x = self.transition2(x)
+        x = self.dense3(x)
+        x = self.transition3(x)
+        x = self.dense4(x)
+        x = self.gap(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
 
         return x
     
 
 def densenet_121(num_classes:int):
-    pass
+    expansions = [6, 12, 24, 16]
+    return DenseNet(expansions, num_classes)
 
 def densenet_169(num_classes:int):
-    pass
+    expansions = [6, 12, 32, 32]
+    return DenseNet(expansions, num_classes)
 
 def densenet_201(num_classes:int):
-    pass
+    expansions = [6, 12, 48, 32]
+    return DenseNet(expansions, num_classes)
 
 def densenet_264(num_classes:int):
-    pass
+    expansions = [6, 12, 64, 48]
+    return DenseNet(expansions, num_classes)
+
+
+image = torch.randn(1, 3, 224, 224)
+net = densenet_121(10)
+print(net(image).size())
