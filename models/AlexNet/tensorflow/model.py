f3209ab5741de08781871df663b729431cc5a197
aledigirm3 <alessandro.digirolamo.1937@gmail.com>
2025-02-05 20:42:27 +0100

    add AlexNet (TF)

---
+++ models/AlexNet/tensorflow/model.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+def AlexNet(num_classes):
+    model = keras.Sequential([
+          
+        layers.Conv2D(96, kernel_size=11, strides=4, input_shape=(227, 227, 3), use_bias=False),
+        layers.BatchNormalization(),
+        layers.ReLU(),
+        layers.MaxPooling2D(pool_size=3, strides=2),
+
+
+        layers.Conv2D(256, kernel_size=3, strides=1, padding=2, use_bias=False),
+        layers.BatchNormalization(),
+        layers.ReLU(),
+        layers.MaxPooling2D(pool_size=3, strides=2),
+
+
+        layers.Conv2D(384, kernel_size=3, strides=1, padding=1, use_bias=False),
+        layers.BatchNormalization(),
+        layers.ReLU(),
+
+
+        layers.Conv2D(384, kernel_size=3, strides=1, padding=1, use_bias=False),
+        layers.BatchNormalization(),
+        layers.ReLU(),
+
+
+        layers.Conv2D(256, kernel_size=3, strides=1, padding=1, use_bias=False),
+        layers.BatchNormalization(),
+        layers.ReLU(),
+        layers.MaxPooling2D(pool_size=3, strides=2),
+
+    
+        layers.Flatten(),
+
+        
+        layers.Dropout(0.5),
+        layers.Dense(4096, activation='relu'),
+
+
+        layers.Dropout(0.5),
+        layers.Dense(4096, activation='relu'),
+        
+        
+        layers.Dense(4096, activation='relu'),
+
+ 
+        layers.Dense(num_classes, activation='softmax')
+    ])
+
+    return model
+
+
+# Test
+num_classes = 1000
+model = AlexNet(num_classes)
+
+model.summary()
