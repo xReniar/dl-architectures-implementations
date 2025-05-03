406f89a93da8587b7f12e5638f4b71b412258bad
xReniar <ilaorainer@gmail.com>
2025-05-04 01:44:33 +0200

    Add DCGAN discriminator implementation

--- DCGAN/discriminator.py
+++ DCGAN/discriminator.py
@@ -1,10 +1,40 @@
-from torch import nn
 import torch
+import torch.nn as nn
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        in_channels: int,
+        feature_map_size: int
+    ) -> None:
         super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, feature_map_size, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(feature_map_size, feature_map_size * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size * 2),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(feature_map_size * 2, feature_map_size * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size * 4),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(feature_map_size * 4, feature_map_size * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size * 8),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(feature_map_size * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
 
     def forward(self, x:torch.Tensor):
-        pass
+        for (_, module) in self.named_children():
+            x = module(x)
+
+        return x
