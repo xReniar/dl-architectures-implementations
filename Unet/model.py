76e373e0f708f3e29e950c5efebca32800658f12
xReniar <ilaorainer@gmail.com>
2025-05-04 01:11:06 +0200

    Add UNet model implementation

---
+++ Unet/model.py
@@ -0,0 +1,23 @@
+from encoder import Encoder
+from decoder import Decoder
+from torch import nn
+import torch
+
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x:torch.Tensor):
+        x, skip_values = self.encoder(x)
+        x = self.decoder(x, skip_values)
+
+        return x
+
+
+model = UNet()
+x = torch.randn(1, 1, 572, 572)
+print(model(x).shape)
