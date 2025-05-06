6c692d0e5d22bdfec939d8fa5c8d10b053e9a92c
xReniar <ilaorainer@gmail.com>
2025-05-06 10:15:28 +0200

    Update train script for DCGAN

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 import torchvision.datasets as dataset
 import torchvision.transforms as transforms
-import numpy as np
+import torchvision.utils as vutils
 
 
 # device selection
@@ -64,8 +64,8 @@ lr = 0.0002
 beta1 = 0.5
 num_epochs = 5
 
-optimzerD = optim.Adam(discriminator.parameters(), lr = lr, betas = (beta1, 0.999))
-optimzerG = optim.Adam(generator.parameters(), lr = lr, betas = (beta1, 0.999))
+optimizerD = optim.Adam(discriminator.parameters(), lr = lr, betas = (beta1, 0.999))
+optimizerG = optim.Adam(generator.parameters(), lr = lr, betas = (beta1, 0.999))
 
 img_list = []
 G_losses = []
@@ -74,4 +74,56 @@ iters = 0
 
 for epoch in range(num_epochs):
     for i, data in enumerate(dataloader, 0):
-        pass
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), 1, dtype=torch.float, device=device)
+
+        output = discriminator(real_cpu).view(-1)
+        errD_real = criterion(output, label)
+
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        noise = torch.randn(b_size, latent_vector_size, 1, 1, device=device)
+
+        fake = generator(noise)
+        label.fill(0)
+
+        output = discriminator(fake.detach()).view(-1)
+
+        errD_fake = criterion(output, label)
+
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+
+        errD = errD_real + errD_fake
+
+        optimizerD.step()
+
+        generator.zero_grad()
+        label.fill_(1)
+
+        output = discriminator(fake).view(-1)
+
+        errG = criterion(output, label)
+
+        errG.backward()
+        D_G_z2 = output.mean().item()
+
+        optimizerG.step()
+
+        # Output training stats
+        if i % 50 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, num_epochs, i, len(dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+            with torch.no_grad():
+                fake = generator(fixed_noise).detach().cpu()
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        iters += 1
