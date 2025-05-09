b47baa3c101ea67de6b9d73d9eb33bd927ae7a4b
xReniar <ilaorainer@gmail.com>
2025-05-09 08:29:02 +0200

    Set bias to False for conv layers and inplace to True for ReLU

--- GoogLeNet/InceptionModule/v1.py
+++ GoogLeNet/InceptionModule/v1.py
@@ -15,31 +15,31 @@ class InceptionModulev1(nn.Module):
         self.pool_proj:int = args[6]
 
         self.branch1 = nn.Sequential(
-            nn.Conv2d(self.in_features, self._1x1, kernel_size=1),
+            nn.Conv2d(self.in_features, self._1x1, kernel_size=1, bias=False),
             nn.BatchNorm2d(self._1x1),
-            nn.ReLU()
+            nn.ReLU(True)
         )
         self.branch2 = nn.Sequential(
-            nn.Conv2d(self.in_features, self._3x3_reduce, kernel_size=1),
+            nn.Conv2d(self.in_features, self._3x3_reduce, kernel_size=1, bias=False),
             nn.BatchNorm2d(self._3x3_reduce),
-            nn.ReLU(),
-            nn.Conv2d(self._3x3_reduce, self._3x3, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(self._3x3_reduce, self._3x3, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(self._3x3),
-            nn.ReLU()
+            nn.ReLU(True)
         )
         self.branch3 = nn.Sequential(
-            nn.Conv2d(self.in_features, self._5x5_reduce, kernel_size=1),
+            nn.Conv2d(self.in_features, self._5x5_reduce, kernel_size=1, bias=False),
             nn.BatchNorm2d(self._5x5_reduce),
-            nn.ReLU(),
-            nn.Conv2d(self._5x5_reduce, self._5x5, kernel_size=5,padding=2),
+            nn.ReLU(True),
+            nn.Conv2d(self._5x5_reduce, self._5x5, kernel_size=5,padding=2, bias=False),
             nn.BatchNorm2d(self._5x5),
-            nn.ReLU()
+            nn.ReLU(True)
         )
         self.branch4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(self.in_features, self.pool_proj, kernel_size=1),
+            nn.Conv2d(self.in_features, self.pool_proj, kernel_size=1, bias=False),
             nn.BatchNorm2d(self.pool_proj),
-            nn.ReLU()
+            nn.ReLU(True)
         )
         self.aux_classifier:nn.Module = args[7]
 
@@ -49,7 +49,8 @@ class InceptionModulev1(nn.Module):
         b3_out = self.branch3(x)
         b4_out = self.branch4(x)
         
+        classifier = None
         if self.aux_classifier != None and mode:
-            self.aux_classifier(x)
+            classifier = self.aux_classifier(x)
 
-        return torch.cat([b1_out, b2_out, b3_out, b4_out], dim=1)
+        return (torch.cat([b1_out, b2_out, b3_out, b4_out], dim=1), classifier)
